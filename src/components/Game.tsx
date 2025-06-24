7a702baaa1aeabd55da17a6fbbdd9f4bd50de1c9
Eason Lee <>
2025-06-24 07:35:33 +0100

    + Updated charactersheet to add hurt and throw anims + Added throw cmd that throws words (that does 10 dmg) + Added stun when enemy hurt that gives invincible frames + Added damage when enemy hurt + Added hurt anim when enemy damaged + Added projectile object (word throw currently) + Added API call to get random word + Seperated functions to appropirately named tsx files + Console commands uses seperate string list to avoid activating states like "idle" or "stunned"

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,73 +1,15 @@
-import {type GameObj, type KAPLAYCtx } from "kaplay";
-import initKaplay from "./KaplayWindow"
+import {type KAPLAYCtx } from "kaplay";
+import initKaplay from "./KaplayWindow";
+import character from "./Character";
+import console from "./Console";
+import projectile from "./Projectile"
+
+
 
 const selectedPlayer= "player1" as string;
-const allowedStates = ["idle", "right", "left", "up", "down"] as string[];
-
-function updateConsole(k: KAPLAYCtx, textInput: GameObj): void
-{
-    textInput.onUpdate(() => {
-        
-        if (k.isKeyPressed("enter")) {
-            const player = k.get(selectedPlayer)[0];
-
-            if (allowedStates.includes(textInput.typedText) && player.state == "idle"){
-
-                player.enterState(textInput.typedText);
-            }
-            textInput.text = "";
-            textInput.typedText = "";
-
-        }
-        
-    })
-
-    
-}
-function initConsole(k: KAPLAYCtx): void
-{
-    // add console bg
-    k.add([
-      k.pos(k.width() / 2, k.height() - 107),
-      k.rect(k.width(), 215),
-      k.color(1,1,1),
-      k.anchor("center")
-
-    ]);
-    k.add([
-      k.pos(k.width() / 2, k.height() - 108),
-      k.rect(1870, 180, { radius: 40 }),
-      k.color(1, 1, 1),
-      k.anchor("center"),
-      k.outline(4, k.GREEN),
-    ]);
-
-    k.add([
-        k.text(">", {font: "dogica-bold"}),
-        k.pos(90, k.height() - 70),
-        k.anchor("left"),
-        k.color(1,255,1)
-
-    ]);
-
-    // text input
-    const textInput = k.add([
-        k.text("", {font: "dogica", size: 30}),
-        k.textInput(true, 10),
-        k.pos(130, k.height() - 72),
-        k.anchor("left"),
-        k.color(1,255,1)
-
-    ]);
-
-    updateConsole(k, textInput);
+const allowedStates = ["idle", "stunned", "right", "left", "up", "down", "throw"] as string[];
 
 
-
-    
-
-
-}
 
 function initFieldCollision(k: KAPLAYCtx) : void
 {
@@ -117,7 +59,7 @@ function initAssets(k : KAPLAYCtx) : void
 {
     // load assets
     k.loadSprite("bg", "./origbig.png")
-    k.loadSprite("character", "./charactersheet.png",{
+    k.loadSprite("character", "./charactersheet2.png",{
         sliceY: 10,
         sliceX: 12,
         anims: {
@@ -127,7 +69,10 @@ function initAssets(k : KAPLAYCtx) : void
             "walk-right": {from: 2, to: 4, loop: false},
             "crouch": {from:72, to: 76, loop: false, speed: 20},
             "uncrouch": {from:76, to: 72, loop: false, speed: 20},
-            "jump": {from:108, to: 117, loop: false}
+            "jump": {from:108, to: 117, loop: false},
+            "throw":{from:77, to: 82, loop:false},
+            "hurt":{from:104, to: 106, loop:false},
+            "hurt-end":{from:106, to: 104, loop:false},
         }
     });
     // load font
@@ -139,156 +84,6 @@ function initAssets(k : KAPLAYCtx) : void
 
 }
 
-function playerUpdate(k: KAPLAYCtx, player: GameObj){
-
-
-    player.onGround(()=>{
-
-        player.enterState("idle");
-
-    })
-    
-    player.onStateUpdate("right", () => {
-         if (player.isGrounded()){
-            player.direction.x = 0;
-            player.direction.y = 0;
-
-            player.direction.x = 1;
-
-            if (player.direction.eq(k.vec2(1,0)))
-            {
-                player.move(player.direction.scale(player.speed));
-                player.play("walk-right");
-
-            }
-         }
-
-
-
-    });
-    player.onStateUpdate("left", () => {
-        if (player.isGrounded()){
-            player.direction.x = 0;
-            player.direction.y = 0;
-            player.direction.x = -1;
-
-            if (player.direction.eq(k.vec2(-1,0)))
-            {
-                player.move(player.direction.scale(player.speed));
-                player.play("walk-left");
-            
-            }
-
-        }
-
-    });
-
-
-    player.onStateEnter("up", async() => {
-
-        if (player.isGrounded()){
-            player.jump(1200);
-            player.play("jump");
-        }
-
-
-    });
-
-
-    player.onStateEnter("down", async() => {
-        if (player.isGrounded()){
-            player.use(k.area({shape: new k.Rect(k.vec2(0,0), 30, 30)}));
-            player.play("crouch");
-
-        }
-
-
-    });
-
-    // idle
-     player.onStateEnter("idle", async() => {
-        player.direction.x = 0;
-        player.direction.y = 0;
-        player.play("idle");
-
-    });
-    player.onAnimEnd(async(anim : string) => {
-
-        switch(anim){
-            case "crouch":
-                player.play("uncrouch");
-                break;
-            case "uncrouch":
-                player.use(k.area({shape: new k.Rect(k.vec2(0,0), 30, 70)}));
-                player.enterState("idle");
-                break;
-            case "dash":
-            case "walk-left":
-            case "walk-right":
-                player.enterState("idle");
-                break;
-
-            
-        }
-
-
-
-        
-
-    });
-
-
-
-}
-
-function initPlayer1(k: KAPLAYCtx)
-{
-    // player left
-    const player = k.add([
-        k.sprite("character", {anim: "idle"}),
-        k.area({shape: new k.Rect(k.vec2(0,0), 30, 70)}),
-        k.body({mass: 1}),
-        k.anchor("bot"),
-        k.pos(100, 500),
-        k.scale(4),
-        k.state("idle", allowedStates),
-        k.health(100),
-        "player1",
-        {
-            speed: 8000,
-            direction: k.RIGHT
-
-        }
-    ]);
-
-
-    
-    return player
-
-}
-
-function initPlayer2(k: KAPLAYCtx)
-{
-    const player2 = k.add([
-      k.sprite("character", { anim: "idle", flipX: true, }),
-      k.area({ shape: new k.Rect(k.vec2(0, 0), 30, 70) }),
-      k.body({ mass: 1 }),
-      k.anchor("bot"),
-      k.pos(1820, 500),
-      k.scale(4),
-      k.health(100),
-      k.state("idle", allowedStates),
-      "player2",
-      {
-        speed: 8000,
-        direction: k.LEFT,
-      },
-    ]);
-
-
-    return player2
-
-}
 
 
 export default function initGame() : void
@@ -299,12 +94,14 @@ export default function initGame() : void
 
     initAssets(k);
     initFieldCollision(k);
-    initConsole(k);
+    
+    console.initConsole(k,selectedPlayer,allowedStates);
 
-    const player1 = initPlayer1(k);
-    const player2 = initPlayer2(k);
-    playerUpdate(k, player1);
-    playerUpdate(k, player2);
+    const player1 = character.initPlayer1(k, allowedStates);
+    const player2 = character.initPlayer2(k, allowedStates);
+    character.playerUpdate(k, player1);
+    character.playerUpdate(k, player2);
+
 
 
     k.debug.inspect = true;
