e6007d8623b6ac96377ff10f8ed0b5bf019c3ac8
Precipitating <92101891+Precipitating@users.noreply.github.com>
2025-06-25 03:14:55 +0100

    + Moved projectile update logic to be done globally + Added grenade command, throws a grenade and explodes a word bomb (360 degree spread) + Added death animation, untagged to prevent checking updates on dead player + Added fetchWords() api command, which fetches x amount of words in 1 call instead of spamming API calls

--- src/components/RandomWord.tsx
+++ src/components/RandomWord.tsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from "react";
-
 const URL = "https://random-word-api.herokuapp.com/word";
 
 export async function fetchWord(): Promise<string> {
@@ -7,4 +5,11 @@ export async function fetchWord(): Promise<string> {
     const data = await res.json();
 
     return data[0];
+}
+
+export async function fetchWords(amount : number): Promise<string[]> {
+    const res = await fetch(URL + "?number=" + amount);
+    const data = await res.json();
+
+    return data;
 }
