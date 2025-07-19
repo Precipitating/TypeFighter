8e3dfd5a7eb0b6add82d0f8109f4c4d9cc41b500
Precipitating <92101891+Precipitating@users.noreply.github.com>
2025-07-19 07:42:05 +0100

    + Fixed endpoint link to server +

--- kaplay-colyseus/client/src/core/colyseus.ts
+++ kaplay-colyseus/client/src/core/colyseus.ts
@@ -1,8 +1,8 @@
 import { Client } from "colyseus.js";
 
-export const SERVER_URL = !import.meta.env.PROD
-    ? `${location.protocol}//${location.host}/colyseus`
-    : "typefighter.ofmdirect.com:2567";
+// export const SERVER_URL = !import.meta.env.PROD
+//     ? `${location.protocol}//${location.host}/colyseus`
+//     : "typefighter.ofmdirect.com";
 
-export const colyseusSDK = new Client("typefighter.ofmdirect.com:2567");
+export const colyseusSDK = new Client("ws://typefighter.ofmdirect.com:2567");
 
