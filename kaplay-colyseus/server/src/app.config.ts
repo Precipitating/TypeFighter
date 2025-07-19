3642c13cf22a638bb6ce69659a7664d035930c5c
Precipitating <92101891+Precipitating@users.noreply.github.com>
2025-07-19 07:27:23 +0100

    Disabled devtools

--- kaplay-colyseus/server/src/app.config.ts
+++ kaplay-colyseus/server/src/app.config.ts
@@ -30,16 +30,16 @@ export default config({
          * Use @colyseus/playground
          * (It is not recommended to expose this route in a production environment)
          */
-        if (process.env.NODE_ENV !== "production") {
-            app.use("/", playground());
-        }
+        // if (process.env.NODE_ENV !== "production") {
+        //     app.use("/", playground());
+        // }
 
         /**
          * Use @colyseus/monitor
          * It is recommended to protect this route with a password
          * Read more: https://docs.colyseus.io/tools/monitor/#restrict-access-to-the-panel-using-a-password
          */
-        app.use("/colyseus", monitor());
+       // app.use("/colyseus", monitor());
     },
 
 
